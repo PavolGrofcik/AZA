955054a29c330974e81920c89700fd1ecbaae582
PavolGrofcik <grofcik.pavol@gmail.com>
2018-04-14 11:08:26 +0200

    Identical

--- AZA Zadanie1/Source.c
+++ AZA Zadanie1/Source.c
@@ -7,381 +7,185 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define NAME "AZA2.in"
+#define NAME "AZA3.in"
 
-//Konštanty pre hash_map
-#define D1  150				//* y - súradnica
-#define D2  22500			//* dx - súradnica
-#define D3  247500			//* dy - súradnica
-#define SIZE  57600000		//Úplná ve¾kos hash map-y
+#define X 151		
+#define Y 151
+#define DX 11
+#define DY 11
 
-
-
-//Štruktúra ktorá reprezentuje bod v mape //prerobi na long
 typedef struct point {
-	int x;
-	int y;
-	int dx;
-	int dy;
-	struct point *parent;
+	short x;
+	short y;
+	short dx;
+	short dy;
+
+	struct point* parent;
 
 }POINT;
 
 
-//Štruktúra, ktorá reprezentuje frontu pre BFS
 typedef struct front {
 	POINT *point;
-	struct front *prev;
-	struct front *next;
+	struct front* next;
 
 }FRONT;
 
 
-//Globálne premenné programu
+//Globálne premenné
+
 char arr[150][150];
+char hashmap[151][151][11][11] = { 0 };
 
-FRONT *head = NULL;
-FRONT *last = NULL;
+FRONT* first;
+FRONT* last;
 
-int *hash_map = NULL;
-
-
-//Funkcia inicializuje hash_map - u
-void initialize_hash_map() {
-
-	hash_map = (FRONT*)calloc(SIZE, sizeof(int));			//Inicializuje hodnoty na 0
-	if (hash_map == NULL) {
-		printf("Nepodarilo sa pridelit pamat\n");
-		return NULL;
-	}
-}
-
-//Funkcia dealokuje hash_map -u
-void deallocate_hash_map() {
-	free(hash_map);
-	hash_map = NULL;
-}
-
-//Funkcia zahashuje danı stav do hash_mapy a urèí èi sa má prida do fronty
-int hashing(int x, int y, int dx, int dy) {
-	int sum;
-	sum = x + D1 * y + D2*(dx+5) + D3*(dx+5);
-
-	if (hash_map[sum] == 0) {								//Danı stav ešte nebol vygenerovanı
-		hash_map[sum] = 1;
+//Funkcia zahashuje danı stav
+int hashing(short x, short y, short dx, short dy) {
+	if (hashmap[x][y][dx + 5][dy + 5] == 0) {
+		hashmap[x][y][dx + 5][dy + 5] = 1;
 		return 1;
 	}
-	else
-	{
-		return 0;		//Danı stav u existuje, nepridám ho do fronty
+	return 0;
+}
+
+//Funkcia obnoví hashmapu na default stav
+void clear_hashmap() {
+	for (int i = 0; i < X; i++) {
+		for (int j = 0; j < Y; j++) {
+			for (int k = 0; k < DX; k++) {
+				for (int z = 0; z < DY; z++) {
+					hashmap[i][j][k][z] = 0;
+				}
+			}
+		}
 	}
 }
 
+
+
+//Funkcia inicializuje frontu
+void initialize_front() {
+	first = (FRONT*)malloc(sizeof(FRONT));
+	first->next = first->point = NULL;
+	last = first;
+}
 //Funkcia dealokuje frontu
 void deallocate_front() {
-	FRONT* tmp = head, *next;
 
-	if (tmp == NULL) {
-		printf("Fronta prázdna\n");
-		return;
-	}
-	else
-	{
-		while (tmp->next != NULL) {
-			next = tmp->next;
-			free(tmp);
-			tmp = next;
+	if (first != NULL) {
+		while (first != NULL) {
+			last = first->next;
+			free(first);
+			first = last;
 		}
-		free(tmp);
-		head = last = NULL;
 	}
+	first = last = NULL;
 }
 
 
-//Funkcia testuje èi je fronta prázdna alebo nie
-int is_empty_front() {
-	if (head == NULL) {
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
-
-
-//Funkcia vytvorí novı uzol do fronty
-FRONT* new_node(POINT *node) {
-	FRONT *tmp = (FRONT*)malloc(sizeof(FRONT));
+//Vytvorenie nového uzla do fronty
+FRONT* new_node(POINT* p) {
+	FRONT* tmp = (FRONT*)malloc(sizeof(FRONT));
 	tmp->next = NULL;
-	tmp->prev = NULL;
-	tmp->point = node;
+	tmp->point = p;
 
 	return tmp;
-
 }
 
-//Funkcia vytvorí novı point
-POINT* new_point(int x, int y, int dx, int dy, POINT *parent) {
+//Funkcia pridá bod do fronty
+void add_to_front(POINT* new) {
+	
 
-	int new_x = parent->x + parent->dx + dx;
-	int new_y = parent->y + parent->dy + dy;
-
-	int new_dx = parent->dx + dx;
-	int new_dy = parent->dy + dy;
-
-	POINT* tmp = (POINT*)malloc(sizeof(POINT));
-
-	tmp->x = new_x;
-	tmp->y = new_y;
-	tmp->dx = new_dx;
-	tmp->dy = new_dy;
-	tmp->parent = parent;
-	printf("Point bol vytvorenı z rodica x:%d y:%d dx:%d dy:%d\tchild dx:%d dy:%d\n", parent->x, parent->y,parent->dx,parent->dy,tmp->dx, tmp->dy);
-
-	return (POINT*)tmp;
-}
-
-//Funkcia slúi na inicializáciu poèiatoèného pointu
-POINT* start_point(int x, int y, int dx, int dy, POINT* parent) {
-
-	POINT* tmp = (POINT*)malloc(sizeof(POINT));
-	tmp->x = x;
-	tmp->y = y;
-	tmp->dx = dx;
-	tmp->dy = dy;
-	tmp->parent = parent;
-
-	return (POINT*)tmp;
-
-}
-
-//Urobi pointer na koniec a na zaèiatok
-void add_to_front(POINT *node) {
-
-	FRONT *tmp = NULL;
-
-	//Pridanie node ak fronta je prázdna
-	if (head == NULL) {
-		head = last = new_node(node);
-		return;
+	if (first == NULL) {
+		first = new_node(new);
+		last = first;
 	}
 	else
 	{
-		tmp = last;
-		tmp->next = new_node(node);
-		tmp->next->prev = tmp;
-
-		//Aktualizovanie posledného
-		last = tmp->next;
+		last->next = new_node(new);
+		last = last->next;
 	}
 }
 
-POINT* pop_from_front() {
-
-	POINT *tmp, *sec;
-	FRONT *iterator, *return_node;
-
-	tmp = (POINT*)malloc(sizeof(POINT));
-
-	//Prvı prvok  vzozname
-	if (head == last) {
-		if (head != NULL) {
-			tmp->dx = head->point->dx;
-			tmp->dy = head->point->dy;
-			tmp->x = head->point->x;
-			tmp->y = head->point->y;
-			tmp->parent = head->point->parent;
-
-			free(head);
-			head = last = NULL;
-			return (POINT*)tmp;
-		}
+//Funkcia extrahuje z fronty
+POINT* extract_from_front() {
+	
+	if (first == NULL) {
 		return NULL;
 	}
 	else
 	{
-
-		//Zefektívnenie
-		iterator = head;
-		head = head->next;
-		head->prev = NULL;
-		return_node = iterator;
-
-		//Prerobi na funkciu
-		tmp->dx = return_node->point->dx;
-		tmp->dy = return_node->point->dy;
-		tmp->x = return_node->point->x;
-		tmp->y = return_node->point->y;
-		tmp->parent = return_node->point->parent;
-
-		//Uvo¾níme poslednı uzol;
-		free(return_node);
-
-		//Posun o jeden uzol spa
+		POINT* tmp = NULL;
+		tmp = first->point;
+		first = first->next;
 		return (POINT*)tmp;
 	}
 }
 
-//Funkcia umocní x na druhú x^2
-int power_of_two(int x) {
-	int number = x * x;
-	if (number < 0) {
-		return number * (-1);
-	}
-	else
-	{
-		return number;
-	}
-}
+//Funkcia overí pohyb panáèika o nové dx a dy
+void validate_move(POINT* p, int dx, int dy, int size_x, int size_y) {
 
-//Funkcia overí èi pre danı pohyb je platnı aj dx2 <= 25
-int is_free(int dx, int dy) {
+	int new_dx = p->dx + dx;
+	int new_dy = p->dy + dy;
 
-	if ((power_of_two(dx) + power_of_two(dy)) <= 25) {
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
+	//Overenie dx a dy <= 25
+	if (((new_dx*new_dx) + (new_dy*new_dy)) <= 25) {
 
-//Funkcia zvaliduje èi panáèik nevyboèil z mapy
-int is_valid_move(POINT* p, int x, int y, int size_x, int size_y) {
-	//int p_x = p->x;	//1
-	//int p_y = p->y;	//18
+		int new_x = p->x + new_dx;
+		int new_y = p->y + new_dy;
 
-	int p_dx = p->dx + x;//-1
-	int p_dy = p->dy + y;//6
-	int p_x;
-	int p_y;
-
-	printf("p_dx: %d p_dy: %d\n", p_dx, p_dy);
-	//Aby sa nešlo do zápornıch indexov
-	if ((p_dx < -5) || (p_dx > 5) || (p_dy > 5) || (p_dy < -5)) {
-		return 0;
-	}
-
-	if (is_free(p_dx, p_dy)) {		//pohyb je <=25
-		//p_x += p_dx;
-		//p_y += p_dy;
-		p_x = p->x + p_dx;
-		p_y = p->y + p_dy;
-
-
-		//Overí èi sa dá prejs cez mapu a cez zakázané políèka
-		if (p_x < 0 || p_x >= size_x || p_y < 0 || p_y >= size_y || arr[p_x][p_y] == 'X') {
-			return 0;
+		//Overenie pohybu panáèiak po mape
+		if (new_x < 0 || new_x >= size_x || new_y < 0 || new_y >= size_y || (arr[new_x][new_y] == 'X')) {
+			return;
 		}
-		else
-		{
-			printf("Preslo: p_dx: %d p_dy: %d p_x: %d p_y: %d\n", p_dx, p_dy,p_x,p_y);
-			return 1;
-		}
-	}
 
-	return 0;
+		//Hashovanie èi sa nachádza danı stav v hash_map-e
+		int sum = hashing(new_x, new_y, new_dx, new_dy);
+		if (sum == 1) {
+			POINT* tmp = (POINT*)malloc(sizeof(POINT));
+			tmp->dx = new_dx;
+			tmp->dy = new_dy;
+			tmp->x = new_x;
+			tmp->y = new_y;
+			tmp->parent = p;
+
+			add_to_front(tmp);
+		}
+		return;
+	}
 }
 
 
-
-//Algoritmus nájdenia najlepšej cesty
-POINT* breadt_first_search(POINT *p, int end_x, int end_y, int size_x, int size_y) {
-
-	POINT* new = NULL;
+POINT* breadth_first_search(POINT* p, int size_x, int size_y, int end_x, int end_y) {
 	POINT* tmp = NULL;
 
-	POINT* valid_point = (POINT*)malloc(sizeof(POINT));		//Valid point
-
+	//initialize_front;
+	first = last = NULL;
 	add_to_front(p);
 
-	while (!is_empty_front()) {
+	while (first != NULL) {
+		tmp = extract_from_front();
 
-		new = pop_from_front();
-
-		//Cesta bola nájdená
-		if (arr[new->x][new->y] == arr[end_x][end_y]) {
-			printf("Koniec\n");
-			return new;
+		if (tmp->x == end_x && tmp->y == end_y) {
+			return tmp;
 		}
 
-		//Pridám novovygenerované stavy do fronty a hash mapy
+		validate_move(tmp, -1, -1, size_x, size_y);
+		validate_move(tmp, -1, 0, size_x, size_y);
+		validate_move(tmp, -1, 1, size_x, size_y);
 
-		//Case dx = -1
-		//-1,-1
-		printf("\nPred pohybom\tx: %d y:%d dx:%d dy:%d\n", new->x, new->y, new->dx, new->dy);
-		if (is_valid_move(new, -1, -1, size_x, size_y)) {
-			if (hashing(new->x + new->dx - 1, new->y + new->dy - 1, new->dx - 1, new->dy - 1)) {
-				tmp = new_point(new->x , new->y,-1, -1, new);
-				add_to_front(tmp);
-			}
-		}//-1,0
-		if (is_valid_move(new, -1,0, size_x, size_y)) {
-			if (hashing(new->x + new->dx - 1, new->y + new->dy, new->dx - 1, new->dy)) {
-				tmp = new_point(new->x , new->y , -1, 0, new);
-				add_to_front(tmp);
-			}
-		}//-1,+1
-		if (is_valid_move(new,-1,  1, size_x, size_y)) {
-			if (hashing(new->x + new->dx - 1, new->y + new->dy + 1, new->dx - 1, new->dy + 1)) {
-				tmp = new_point(new->x, new->y, - 1, 1, new);
-				add_to_front(tmp);
-			}
-		}
-		printf("Po prvych 3\tx: %d y:%d dx:%d dy:%d\n", new->x, new->y, new->dx, new->dy);
-		//Case: dx = 0
-		//0,-1
-		if (is_valid_move(new, 0, - 1, size_x, size_y)) {
-			if (hashing(new->x + new->dx, new->y + new->dy - 1, new->dx, new->dy - 1)) {
-				tmp = new_point(new->x , new->y, 0, -1, new);
-				add_to_front(tmp);
-			}
-		}//0,0
-		if (is_valid_move(new,0,0, size_x, size_y)) {
-			if (hashing(new->x + new->dx, new->y + new->dy, new->dx, new->dy)) {
-				tmp = new_point(new->x, new->y, 0, 0, new);
-				add_to_front(tmp);
-			}
-		}
-		//0,1
-		if (is_valid_move(new, 0, 1, size_x, size_y)) {
-			if (hashing(new->x + new->dx, new->y + new->dy + 1, new->dx, new->dy + 1)) {
-				printf("Som tuuuu\n");
-				tmp = new_point(new->x, new->y, 0, 1, new);
-				add_to_front(tmp);
-			}
-		}
+		validate_move(tmp, 0, -1, size_x, size_y);
+		validate_move(tmp, 0, 0, size_x, size_y);
+		validate_move(tmp, 0, 1, size_x, size_y);
 
-		printf("Po druhych 3\tx: %d y:%d dx:%d dy:%d\n", new->x, new->y, new->dx, new->dy);
-		//Case: dx = +1
-		//1,-1
-		if (is_valid_move(new,1,-1, size_x, size_y)) {
-			if (hashing(new->x + new->dx + 1, new->y + new->dy - 1, new->dx + 1, new->dy - 1)) {
-				tmp = new_point(new->x, new->y,1,-1, new);
-				add_to_front(tmp);
-			}
-		}//1,0
-		if (is_valid_move(new,1, 0, size_x, size_y)) {
-			if (hashing(new->x + new->dx + 1, new->y + new->dy, new->dx + 1, new->dy)) {
-				tmp = new_point(new->x, new->y,1,0, new);
-				add_to_front(tmp);
-			}
-		}//1,1
-
-		if (is_valid_move(new, 1, 1, size_x, size_y)) {
-			if (hashing(new->x + new->dx + 1, new->y + new->dy + 1, new->dx + 1, new->dy + 1)) {
-				tmp = new_point(new->x, new->y,1,1, new);
-				add_to_front(tmp);
-			}
-		}
-		printf("Po iterácii\tx: %d y:%d dx:%d dy:%d\n", new->x, new->y, new->dx, new->dy);
-
+		validate_move(tmp, 1, -1, size_x, size_y);
+		validate_move(tmp, 1, 0, size_x, size_y);
+		validate_move(tmp, 1, 1, size_x, size_y);
 	}
-
-	//Ak sa riešenie nenašlo
 	return NULL;
 }
+
 
 
 void vypis_labyrintu(int x, int y) {
@@ -393,7 +197,6 @@ void vypis_labyrintu(int x, int y) {
 		}
 		putchar('\n');
 	}
-	putchar('\n');
 }
 
 
@@ -401,10 +204,7 @@ int main(void) {
 
 	int scenario, x, y, start_x, start_y, end_x, end_y, c, i, j;
 	FILE *f = NULL;
-
-	POINT* p = NULL, *tmp, *next;
-	POINT *path = NULL;
-
+	POINT *path = NULL, *tmp = NULL, *p = NULL;
 
 	if ((f = fopen(NAME, "r")) == NULL) {				//Otvorenie súboru pre naèítavanie
 		printf("Nepodarilo sa otvorit subor\n");
@@ -412,18 +212,18 @@ int main(void) {
 	}
 
 
-	while (!feof(f)) {									//Naèítavanie scenárov zo súboru
+	while (!feof(f)) {								//Naèítavanie scenárov zo súboru
 		fscanf(f, "%d\n\n", &scenario);
 
 		while (scenario > 0) {
-			fscanf(f, "%d", &x);			//Rozmer x po¾a
-			fscanf(f, "%d\n", &y);			//Rozmer y po¾a
+			fscanf(f, "%d", &x);					//Rozmer x po¾a
+			fscanf(f, "%d\n", &y);					//Rozmer y po¾a
 
-			fscanf(f, "%d", &start_x);		//Zaèiatok x v poli
-			fscanf(f, "%d\n", &start_y);	//Zaèiatok y v poli
+			fscanf(f, "%d", &start_x);				//Zaèiatok x v poli
+			fscanf(f, "%d\n", &start_y);			//Zaèiatok y v poli
 
-			fscanf(f, "%d", &end_x);		//Koniec x v poli
-			fscanf(f, "%d\n", &end_y);		//Koniec y v poli
+			fscanf(f, "%d", &end_x);				//Koniec x v poli
+			fscanf(f, "%d\n", &end_y);				//Koniec y v poli
 
 			for (i = 0; i < x; i++) {
 				for (j = 0; j < y; j++) {
@@ -439,40 +239,51 @@ int main(void) {
 				}
 			}
 
-			//Some code snippet of BFS
-			vypis_labyrintu(x, y);
-			//Úprava labyrintu cie¾ového bodu na písmeno Z
-			arr[end_x][end_y] = 'Z';
+			p = (POINT*)malloc(sizeof(POINT));
+			p->dx = 0;
+			p->dy = 0;
+			p->x = start_x;
+			p->y = start_y;
+			p->parent = NULL;
+			hashing(p->x, p->y, p->dx, p->dy);
+			
+			//vypis_labyrintu(x, y);
+			path = breadth_first_search(p, x, y, end_x, end_y);
 
+			if (path!=NULL) {
+				while (path->parent != NULL) {
+					arr[path->x][path->y] = 'o';
+					tmp = path;
+					path = path->parent;
+					free(tmp);
+				}
+				arr[p->x][p->y] = 'o';
+				
+				//Vıpis mapy
+				for (int i = 0; i < x; i++) {
+					for (int j = 0; j < y; j++) {
+						if (arr[i][j] == 46) {
+							putchar(' ');
+						}
+						else
+						{
+							printf("%c", arr[i][j]);
+						}
 
-			//Inicializácia hashmapy
-			initialize_hash_map();
-
-
-			//Volanie BFS
-			//Vytvorenie inicializaèného bodu
-			p = start_point(start_x, start_y, 0, 0, NULL);
-			hashing(start_x, start_y, 0, 0);
-
-			path = breadt_first_search(p, end_x, end_y, x, y);
-
-			if (path == NULL) {
-				printf("No solution\n");
+					}
+					putchar('\n');
+				}
 			}
 			else
 			{
-				//Tu bude vıpis cesty cez dva for-y (funkcia vypis_riesenia())
-				printf("Solution was found!\n");
-				while (path->parent != NULL) {
-					printf("x:%d y:%d dx:%d, dy:%d\n", path->x, path->y, path->dx, path->dy);
-					path = path->parent;
-				}
-				printf("x:%d y:%d dx:%d, dy:%d\n", p->x, p->y, p->dx, p->dy);
+				printf("No solution\n");
 			}
+			free(p);
+			p = NULL;
+			clear_hashmap();
+			deallocate_front();
 
 			printf("--------------------\n");
-			deallocate_front();
-			deallocate_hash_map();
 			scenario--;
 		}
 	}
